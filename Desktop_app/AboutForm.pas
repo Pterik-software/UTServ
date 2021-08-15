d1b798c8bfb7802f00362878169e812b2b0a2c5d
pterik <pterik@gmail.com>
2021-08-16 00:12:52 +0300

    добавляем_договора

---
+++ Desktop_app/AboutForm.pas
@@ -0,0 +1,32 @@
+unit AboutForm;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Buttons;
+
+type
+  TFormAbout = class(TForm)
+    BitBtnClose: TBitBtn;
+    Memo1: TMemo;
+    procedure BitBtnCloseClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FormAbout: TFormAbout;
+
+implementation
+
+{$R *.dfm}
+
+procedure TFormAbout.BitBtnCloseClick(Sender: TObject);
+begin
+Close;
+end;
+
+end.
